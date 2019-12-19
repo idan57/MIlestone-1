e0ed7b7e60bdacd182523afe6cb33beb065e1355
idan57 <47928001+idan57@users.noreply.github.com>
2019-12-19 19:47:48 +0200

    Update Var.h

--- Expressions/Var.h
+++ Expressions/Var.h
@@ -6,21 +6,36 @@
 #define MILESTONE_1_VAR_H
 
 #include <iostream>
+#include <mutex>
+
 using namespace std;
 
 class Var {
 private:
     string* path;
     double value;
-
+    mutex locker;
     // Will have the expression that we will have to evaluate
     //Expression expression;
 public:
     Var(string* p, double val):path(p),value(val){}
     void SetPath(string* p) {this->path = p;}
     string* GetPath() { return this->path;}
-    void SetValue(double val) {this->value = val;}
-    int GetValue() {return this->value;}
+    void SetValue(double val) {
+        locker.lock();
+        this->value = val;
+        locker.unlock();
+
+    }
+    int GetValue() {
+        bool succeededLocking = false;
+        while(!(succeededLocking)) {
+            succeededLocking = locker.try_lock();
+        }
+        double val = this->value;
+        locker.unlock();
+        return val;
+    }
 };
 
 
