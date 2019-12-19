f6971db4c0fa02a7ba728ee208d5d7d983cfec0d
liad2508 <58739913+liad2508@users.noreply.github.com>
2019-12-19 16:54:11 +0200

    Update interpreter.h

--- Interpreter/interpreter.h
+++ Interpreter/interpreter.h
@@ -1,1 +1,23 @@
+//
+// Created by liad on 15/12/2019.
+//
+
+#ifndef MILESTONE_1_INTERPRETER_H
+#define MILESTONE_1_INTERPRETER_H
+#include <bits/stdc++.h>
+using namespace std;
+
+class interpreter {
+    vector <string>* tokens;
+    string filename;
+
+public:
+
+    interpreter(string name);
+    void lexer();
+    vector<string> *getTokens() const;
+};
+
+
+#endif //MILESTONE_1_INTERPRETER_H
 
