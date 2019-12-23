cb2446d6ed72d86aafe8a4e6f2c3d62546ff22f0
liad2508 <58739913+liad2508@users.noreply.github.com>
2019-12-23 14:56:43 +0200

    Update Command.cpp

--- Commands/Command.cpp
+++ Commands/Command.cpp
@@ -1,21 +1,20 @@
-
+// Parses the text we lexered beforehand until the index n.
 #include "Command.h"
 
-// Parses the text we lexered beforehand until the index n.
-vector<string*>* Command::parse(int n) {
 
+
+
+vector<string>* Command::parse(int n) {
     // The parsed data in a vector.
-    vector<string*>* parsed = new vector<string*>;
-    int i = 1;
-
-    // Iterating
+    vector<string>* parsed = new vector<string>;
+    int i = 0;
     for(auto curr = (*this->interperted).begin(); i <= n; i++, curr++) {
         parsed->push_back(*curr);
+        (*this->interperted).erase(this->interperted->begin());
     }
     return parsed;
 }
 
-// Splitting data by a delimeter
 vector<double> * Command::splitNums(char* data, char delimeter) {
     int i = 0;
     stringstream number;
@@ -44,27 +43,62 @@ void Command::UpdateVariables(vector<double>* updatedVars, char SerOrCli) {
     if (SerOrCli == 's') {
         int serverLen = this->ServerUpdate->size();
         for(int i = 0; i < serverLen; i++) {
-            // Dir to update
             string* dirToUpdate = (*this->ServerUpdate)[i];
-
-            // Get the location in the vector that is needed to be updated
-            int locationOfNewVal = (*this->directories)[dirToUpdate];
-
-            // Update Variable
-            (*this->variables)[dirToUpdate]->SetValue
-            ((*updatedVars)[locationOfNewVal]);
-        }
-    }
-
-    // Update Client
-    if (SerOrCli == 'c') {
-        int serverLen = this->ClientUpdate->size();
-        for(int i = 0; i < serverLen; i++) {
-            string* dirToUpdate = (*this->ClientUpdate)[i];
             int locationOfNewVal = (*this->directories)[dirToUpdate];
             (*this->variables)[dirToUpdate]->SetValue
                     ((*updatedVars)[locationOfNewVal]);
         }
     }
+
+    // Update Client
+    if (SerOrCli == 'c') {
+
+    }
     locker.unlock();
 }
+
+
+bool Command::Condition(double var, double val, string op) {
+    if (op == "==") {
+        return var == val;
+    }
+    else if (op == "!=") {
+        return var != val;
+    }
+    else if (op == ">=") {
+        return var >= val;
+    }
+    else if (op == "<=") {
+        return var <= val;
+    }
+    else if (op == "<") {
+        return var < val;
+    }
+    else if (op == ">") {
+        return var > val;
+    }
+
+}
+
+Interpreter* Command::setVar() {
+
+    Interpreter *i = new Interpreter();
+    string tempVar = nullptr;
+    string setVar = "";
+    auto itr = this->variables->begin();
+    while (itr != this->variables->end()) {
+        tempVar = *itr->second->GetPath() + "=" + to_string((itr->second->GetValue())) + ";";
+        setVar += tempVar;
+        itr++;
+    }
+    i->setVariables(setVar);
+    return i;
+}
+
+
+
+
+
+
+
+
