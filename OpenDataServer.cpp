a1cee6eb1c3ba262f15a00631599a20e9077e4cc
idan57 <47928001+idan57@users.noreply.github.com>
2019-12-29 13:40:01 +0200

    Add files via upload

---
+++ OpenDataServer.cpp
@@ -0,0 +1,54 @@
+//
+// Created by idan on 12/12/2019.
+//
+
+#include "OpenDataServer.h"
+#include "../Extra Methods/Extra.h"
+
+int OpenDataServer::execute() {
+
+    // Parse the vector of parsed data
+    vector<string>* parsed = this->parse(1);
+    // Get The port's value
+    int port = this->setVar(parsed->at(1))->calculate();
+
+    // Open server as a thread
+    thread serverOpen(&OpenDataServer::OpenServerConnection, this, port);
+
+    // Wait until server is open and in reading mode
+    serverOpen.join();
+    return 1;
+}
+
+void OpenDataServer::OpenServerConnection(int port) {
+
+    // Create server instance
+    this->serverConnection = new server(port);
+
+    // Create server connection
+    int server_socket = this->serverConnection->CreateServer();
+
+    this->serverConnection->CreateConnection();
+}
+
+void OpenDataServer::ReadingMode(int client_connected ,bool*
+there_are_more_commands) {
+    char dataFromSim[1024] = {0};
+    while(*there_are_more_commands) {
+        int bytesRead = read(client_connected , dataFromSim, 1024);
+        //cout << dataFromSim << endl;
+        ChangeMap(dataFromSim);
+    }
+    this->serverConnection->close();
+}
+void OpenDataServer::ChangeMap(char* dataFromSim) {
+
+    // Split data by ','
+    vector<double>* dataToUpdate = splitNums(dataFromSim, ',');
+
+
+    // Update the variables accordingly
+    this->UpdateVariables(dataToUpdate, 's');
+}
+
+
